9644367467cd06556c53c32fc84e5c49c9ee4ba1
Alice Void <hypnocarrot@csu.fullerton.edu>
2024-05-02 23:44:42 -0700

    improved the score detection model

--- score.py
+++ score.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pytesseract  
 import cv2
 import os
+import numpy as np
 
 CSV_PATH = 'dataset/gameStates/scores.csv'
 
@@ -12,11 +13,27 @@ def get_score(image_path):
         raise ValueError("Could not load the image. Check the file path.")
     
     # Crop to get only the top-left corner
-    cropped_image = image[100:250, :350]
-    resized_image = cv2.resize(cropped_image, (350, 150))
+    img = image[100:250, :300]
+    img = cv2.resize(img, (300, 150))
+
+    #  Preprocessing: 
+        # Grayscale -> Thresholding -> Sharpening -> Erosion -> Dilation -> Gaussian Blur
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    img = cv2.filter2D(img, -1, np.array([[0, -1, 0], [-1, 5,-1], [0, -1, 0]]))
+    img = cv2.erode(img, np.ones((3, 3), np.uint8), iterations=1)
+    img = cv2.dilate(img, np.ones((3, 3), np.uint8), iterations=1)
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    
+    # Display the Image (DEBUGGING PURPOSES)
+    # cv2.imshow('image', img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
     # Debugging w/ PyTesseract Checking
-    score = pytesseract.image_to_string(resized_image, config='--psm 9 outputbase digits')
+    score = pytesseract.image_to_string(img, config='--psm 6 --oem 3 outputbase digits')
+    if score == '':
+        return '0'
     return score
 
 # Get Test Scores 
@@ -35,4 +52,12 @@ def compare_scores(csv_path):
     for image_name, score in test_scores:
         image_path = os.path.join('dataset', 'gameStates', image_name + ".png")
         detected_score = get_score(image_path)
-        print(f"Image: {image_name}, Actual Score: {score}, Detected Score: {detected_score}")
+        
+        #  Printing Comparisson
+        if int(score) == int(detected_score):
+            print(f"Image: {image_name}, Score: {score}")
+        else:
+            print(f"Image: {image_name}, Actual Score: {score}, Detected Score: {detected_score}")
+
+# Testing Code
+compare_scores(CSV_PATH)
